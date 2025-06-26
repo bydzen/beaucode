4ebc810152a0b7d4a5135d42a80d454d350fcf2b
bydzen <svnteentthones@gmail.com>
2025-06-26 18:13:59 +0700

    formsmonule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+export const appConfig = {
+  providers: [
+    importProvidersFrom(FormsModule),
+    provideHttpClient(),
+    provideRouter([]),
+    provideAnimations(),
+  ],
 };
